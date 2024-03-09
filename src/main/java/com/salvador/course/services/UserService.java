07d989cf6c0406674a2d18e0e50396eddf500b02
Joao Salvador <salvador444sj@gmail.com>
2024-03-08 21:57:03 -0300

    Exception handling - update

--- src/main/java/com/salvador/course/services/UserService.java
+++ src/main/java/com/salvador/course/services/UserService.java
@@ -12,6 +12,8 @@ import com.salvador.course.repositories.UserRepository;
 import com.salvador.course.services.exceptions.DatabaseException;
 import com.salvador.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -43,9 +45,13 @@ public class UserService {
 	}
 
 	public User update(Long id, User obj) {
-		User entity = findById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {	
+			User entity = findById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
