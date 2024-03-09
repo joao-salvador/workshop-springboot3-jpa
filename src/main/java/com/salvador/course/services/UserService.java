7955628ec06b4967e95f5f7624384a17575b5e5c
Joao Salvador <salvador444sj@gmail.com>
2024-03-08 21:47:26 -0300

    Exception handling - delete

--- src/main/java/com/salvador/course/services/UserService.java
+++ src/main/java/com/salvador/course/services/UserService.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.salvador.course.entities.User;
 import com.salvador.course.repositories.UserRepository;
+import com.salvador.course.services.exceptions.DatabaseException;
 import com.salvador.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -15,24 +17,31 @@ public class UserService {
 
 	@Autowired
 	private UserRepository repository;
-	
+
 	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
-	
+
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
-	
+
 	public void delete(Long id) {
-		repository.deleteById(id);
+		if (!repository.existsById(id)) {
+			throw new ResourceNotFoundException(id);
+		}
+		try {
+			repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
-	
+
 	public User update(Long id, User obj) {
 		User entity = findById(id);
 		updateData(entity, obj);
